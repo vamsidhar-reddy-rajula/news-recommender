9215bb7f763243a5ea0dcabf590f98dee304e989
vamsidhar Reddy Rajula <vamsidhar_reddy_rajula@protonmail.com>
2021-08-31 02:59:18 -0700

    ensemble,weightage to published date and headline

--- src/api/app.py
+++ src/api/app.py
@@ -4,6 +4,7 @@ from flask_restful import Api, Resource
 import joblib
 import numpy as np
 import pandas as pd
+import contractions
 
 from nltk.tokenize import word_tokenize
 from string import punctuation
@@ -22,7 +23,7 @@ from sklearn.metrics.pairwise import cosine_similarity
 app = Flask(__name__)
 api = Api(app)
 
-df_articles = pd.read_csv("data/interim/articles_processed.csv")
+df_articles = pd.read_csv("../data/interim/articles_processed.csv")
 df_articles.article_published_on = df_articles.article_published_on.astype(
     np.datetime64
 )
@@ -34,9 +35,6 @@ df_train, df_test = (
         df_articles.article_published_on >= datetime(year=2021, day=20, month=8)
     ],
 )
-vectorizer = joblib.load("models/vectorizer_0830_0350")
-model = joblib.load("models/lda_model_0830_0350")
-topic_vectors_train = joblib.load("models/topic_vector_train_0830_0350")
 
 
 def text_pipeline(X):
@@ -49,8 +47,19 @@ def text_pipeline(X):
             f"Input should either be in 'str' format or a 'series' or 'Dataframe' with a column of text. Received an object of type {type(X)}"
         )
 
+    expanded_contractions = X.apply(lambda x: contractions.fix(x))
+
+    lower = expanded_contractions.str.lower()
+
+    custom_preprocessor = lower.apply(
+        lambda x: x.replace("-", " ")
+        .replace("'s", "")
+        .replace("’s", "")
+        .replace("–", "")
+    )
+
     # punctuations
-    removed_punctuation = X.apply(
+    removed_punctuation = custom_preprocessor.apply(
         lambda x: "".join([c for c in x if c not in punctuation])
     )
 
@@ -105,39 +114,183 @@ def get_topic_vectors(X, model, fit=False):
         return model.transform(X)
 
 
-def get_similar_articles(text_vectors, X, top_n_values=10):
+def get_saved_models(n_components):
+    if n_components == 300:
+        topic_vectors_train = joblib.load(f"../models/vectorizer_0830_1513_300")
+        vectorizer = joblib.load(f"../models/lda_model_0830_1513_300")
+        model = joblib.load(f"../models/topic_vector_train_0830_1513_300")
+    elif n_components == 240:
+        topic_vectors_train = joblib.load(f"../models/vectorizer_0830_1406_240")
+        vectorizer = joblib.load(f"../models/lda_model_0830_1406_240")
+        model = joblib.load(f"../models/topic_vector_train_0830_1406_240")
+    elif n_components == 180:
+        topic_vectors_train = joblib.load(f"../models/vectorizer_0830_1304_180")
+        vectorizer = joblib.load(f"../models/lda_model_0830_1304_180")
+        model = joblib.load(f"../models/topic_vector_train_0830_1304_180")
+    elif n_components == 150:
+        topic_vectors_train = joblib.load(f"../models/vectorizer_0830_1205_150")
+        vectorizer = joblib.load(f"../models/lda_model_0830_1205_150")
+        model = joblib.load(f"../models/topic_vector_train_0830_1205_150")
+    elif n_components == 120:
+        topic_vectors_train = joblib.load(f"../models/vectorizer_0830_1109_120")
+        vectorizer = joblib.load(f"../models/lda_model_0830_1109_120")
+        model = joblib.load(f"../models/topic_vector_train_0830_1109_120")
+    elif n_components == 90:
+        topic_vectors_train = joblib.load(f"../models/vectorizer_0830_1015_90")
+        vectorizer = joblib.load(f"../models/lda_model_0830_1015_90")
+        model = joblib.load(f"../models/topic_vector_train_0830_1015_90")
+    elif n_components == 60:
+        topic_vectors_train = joblib.load(f"../models/vectorizer_0830_0925_60")
+        vectorizer = joblib.load(f"../models/lda_model_0830_0925_60")
+        model = joblib.load(f"../models/topic_vector_train_0830_0925_60")
+    elif n_components == 30:
+        topic_vectors_train = joblib.load(f"../models/vectorizer_0830_0838_30")
+        vectorizer = joblib.load(f"../models/lda_model_0830_0838_30")
+        model = joblib.load(f"../models/topic_vector_train_0830_0838_30")
+    return topic_vectors_train, vectorizer, model
+
+
+components_saved = [300, 240, 180, 150, 120, 90, 60, 30]
+
+
+def get_similar_articles(similarity_scores, top_n_values=5):
+    results = []
+    values = np.sort(similarity_scores, axis=0)[::-1, :][:top_n_values, :]
+    similarity_array = np.argsort(similarity_scores, axis=0)[::-1, :][:top_n_values, :]
+    for i in range(similarity_array.shape[1]):
+        indices = similarity_array[:, i]
+        data = pd.DataFrame(
+            {
+                "article_heading": df_train.iloc[indices].article_heading,
+                "Similarity score": values[:, i],
+                "article_date": df_train.iloc[indices].article_published_on,
+            }
+        )
+        for _, row in data.iterrows():
+            results.append(row.to_dict())
+    return results
+
+
+def get_article_importance_day_wise(growth=1000):
+    diff_from_max_date = (
+        df_train.article_published_on - (df_train.article_published_on.max())
+    ).dt.days
+    return np.exp(diff_from_max_date / growth)
+
+
+def get_similarity_score(text_vectors, X, factor=None):
     """
     Evalute the cosine similarity between provided 'text_vectors' and trained X (articles trained and stored as a vecotr of topics).
     Return dataframe with index as trained articles and columns as text_vector indices with values as similarity scores
     """
     similarity_scores = cosine_similarity(X, text_vectors, dense_output=True)
-    return np.argsort(similarity_scores, axis=0)[::-1, :][:top_n_values, :]
+    return similarity_scores * factor
 
 
-def get_result_dict(X):
-    result = {}
-    test_lemmas = text_pipeline(X)
+#     return np.argsort(similarity_scores, axis=0)[::-1,:][:top_n_values,:]
+
+
+def process_news_article(
+    component,
+    include_headings=False,
+    heading_weightage=0.6,
+    test_indices=None,
+    factor=np.ones((df_train.shape[0])).reshape(-1, 1),
+):
+    topic_vectors_train, vectorizer, model = get_saved_models(component)
+    test_lemmas = text_pipeline(df_test.iloc[test_indices].article_body)
     lemma_test_vectors = text_vectorizer(test_lemmas, vectorizer)
     topic_vectors_test = get_topic_vectors(lemma_test_vectors, model)
-    similarity_scores = get_similar_articles(topic_vectors_test, topic_vectors_train)
-    for i in range(similarity_scores.shape[0]):
-        headline = df_train.iloc[similarity_scores[i]].article_heading.iloc[0]
-        url = df_train.iloc[similarity_scores[i]].article_url.iloc[0]
-        result[headline] = url
-    return result
-
-
-def print_similar_articles(test_indices, test, df_train, similarity_array):
-    for i in range(similarity_array.shape[1]):
-        indices = similarity_array[:, i]
-        print("\n")
-        print(test.iloc[test_indices[i]].article_heading)
-        print("\n")
-        print(
-            df_train.iloc[indices]
-            .sort_values(["article_published_on"], ascending=False)
-            .article_heading
+    similarity_scores = get_similarity_score(
+        topic_vectors_test, topic_vectors_train, factor=factor
+    )
+    heading, result = get_similar_articles(test_indices, similarity_scores)
+    if include_headings:
+        test_lemmas = text_pipeline(df_test.iloc[test_indices].article_heading)
+        lemma_test_vectors = text_vectorizer(test_lemmas, vectorizer)
+        topic_vectors_test = get_topic_vectors(lemma_test_vectors, model)
+        similarity_scores = (
+            heading_weightage
+            * (
+                get_similarity_score(
+                    topic_vectors_test, topic_vectors_train, factor=factor
+                )
+            )
+            + (1 - heading_weightage) * similarity_scores
         )
+        _, result = get_similar_articles(test_indices, similarity_scores)
+    return heading, result
+
+
+def ensemble_similarity_scores(
+    heading=None,
+    text=None,
+    components=None,
+    include_headings=False,
+    heading_weightage=0.6,
+    factor=np.ones((df_train.shape[0])).reshape(-1, 1),
+):
+    component_similarity_scores = []
+    for component in components:
+        topic_vectors_train, vectorizer, model = get_saved_models(component)
+        test_lemmas = text_pipeline(text)
+        lemma_test_vectors = text_vectorizer(test_lemmas, vectorizer)
+        topic_vectors_test = get_topic_vectors(lemma_test_vectors, model)
+        similarity_scores = get_similarity_score(
+            topic_vectors_test, topic_vectors_train, factor=factor
+        )
+        if include_headings:
+            test_lemmas = text_pipeline(heading)
+            lemma_test_vectors = text_vectorizer(test_lemmas, vectorizer)
+            topic_vectors_test = get_topic_vectors(lemma_test_vectors, model)
+            similarity_scores = (
+                heading_weightage
+                * (
+                    get_similarity_score(
+                        topic_vectors_test, topic_vectors_train, factor=factor
+                    )
+                )
+                + (1 - heading_weightage) * similarity_scores
+            )
+        component_similarity_scores.append(similarity_scores)
+    return component_similarity_scores
+
+
+# def get_result_dict(test_indices, similarity_scores, top_n_values=10):
+#     result = {}
+#     for i in range(similarity_scores.shape[0]):
+#         headline = df_train.iloc[similarity_scores[i]].article_heading.iloc[0]
+#         url = df_train.iloc[similarity_scores[i]].article_url.iloc[0]
+#         result[headline] = url
+#     return result
+
+
+def get_results(heading=None, text=None):
+    factor = np.ones((df_train.shape[0])).reshape(-1, 1)
+    # weights = np.random.dirichlet(np.ones(8),size=1).reshape(8,)
+    weights = [
+        0.0392685,
+        0.09838475,
+        0.04760199,
+        0.05147573,
+        0.04382252,
+        0.0741635,
+        0.04844888,
+        0.59683412,
+    ]
+    # factor = get_article_importance_day_wise(growth=1000).values.reshape(-1,1)
+    component_similarity_scores = ensemble_similarity_scores(
+        heading=heading,
+        text=text,
+        components=components_saved,
+        include_headings=True,
+        heading_weightage=0.75,
+        factor=factor,
+    )
+    similarity_scores = np.average(
+        np.array(component_similarity_scores), axis=0, weights=weights
+    )
+    return get_similar_articles(similarity_scores, top_n_values=10)
 
 
 class Recommender(Resource):
@@ -146,8 +299,9 @@ class Recommender(Resource):
         Default page
         """
         args = request.args
-        test = args["test"]
-        return jsonify(get_result_dict(test))
+        heading = args["heading"]
+        text = args["text"]
+        return jsonify(get_results(heading=heading, text=text))
 
     def post(self):
         """
