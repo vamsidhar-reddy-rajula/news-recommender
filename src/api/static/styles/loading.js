0ec34cb8ab08c32e813cd418602fba5d94a10646
vamsidhar Reddy Rajula <vamsidhar_reddy_rajula@protonmail.com>
2021-09-03 05:41:56 -0700

    added form validation

--- src/api/static/styles/loading.js
+++ src/api/static/styles/loading.js
@@ -1,4 +1,39 @@
-function loading(){
-    document.getElementById("content").style.display = 'none';
-    document.getElementById("loading").style.display = 'block';      
+function loading_submit(){
+    var article_body = document.getElementById('article-body').value;
+    var headline = document.getElementById('headline').value;
+
+    if((headline.length!=0 || headline) && (article_body.length!=0 || article_body)){
+        document.getElementById("content").style.display = 'none';
+        document.getElementById("loading").style.display = 'block';  
+    }    
 }
+
+function loading_random(){
+
+        document.getElementById("content").style.display = 'none';
+        document.getElementById("loading").style.display = 'block';  
+  
+}
+
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+function validation() {
+    'use strict'
+  
+    // Fetch all the forms we want to apply custom Bootstrap validation styles to
+    var forms = document.querySelectorAll('.needs-validation')
+  
+    // Loop over them and prevent submission
+    Array.prototype.slice.call(forms)
+      .forEach(function (form) {
+        form.addEventListener('submit', function (event) {
+        var headline = document.getElementById('headline').value;
+        var article_body = document.getElementById('article-body').value;
+          if (!form.checkValidity() || ((headline.length==0 || !headline) && (article_body.length==0 || !article_body))) {
+            event.preventDefault()
+            event.stopPropagation()
+          }
+  
+          form.classList.add('was-validated')
+        }, false)
+      })
+  }
